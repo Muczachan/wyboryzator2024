f70845471faa3e2ceae009f311b80d9a06f6e01e
Artur Stępień <muczachan@gmail.com>
2026-07-09 19:47:57 +0200

    fixed vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
-  base: './',
+  base: '/wyboryzator2024/',
   plugins: [preact()],
   test: { environment: 'node' },
 });
